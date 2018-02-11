5b1acc97331f16fc79e313e2794597c697482507
Andrei Dziaineka <dziaineka@gmail.com>
2018-02-11 16:21:13 +0300

    refactoring

--- js/cities_loader.js
+++ js/cities_loader.js
@@ -1,14 +1,14 @@
 // города запрашиваем полным списком до начала игры
 // так мы сможем продолжить игру в случае потери связи
 
-var cityData;
+let cityData;
 
 function getJSON(url, callback) {
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
-  xhr.onload = function () {
-    var status = xhr.status;
+  xhr.onload = () => {
+    const status = xhr.status;
     if (status === 200) {
       callback(null, xhr.response);
     } else {
@@ -18,15 +18,17 @@ function getJSON(url, callback) {
   xhr.send();
 }
 
-getJSON('https://gist.githubusercontent.com/dziaineka/edac9ff93e3b3ddf64cad3ec25fdceff/raw/24ff99f23f9ccc917b08dc7f78a098ee7425b1a7/cities.json',
-  function (err, data) {
+getJSON(
+  'https://gist.githubusercontent.com/dziaineka/edac9ff93e3b3ddf64cad3ec25fdceff/raw/24ff99f23f9ccc917b08dc7f78a098ee7425b1a7/cities.json',
+  (err, data) => {
     if (err !== null) {
       alert('При загрузке списка городов что-то пошло не так: ' + err);
     } else {
       cityData = data;
 
-      cityData.forEach(function (item, i, arr) {
+      cityData.forEach((item, i, arr) => {
         arr[i] = item.toLowerCase();
       });
     }
-  });
+  }
+);
