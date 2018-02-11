f70b687deefd31e5a190b794b1c4c2c6f5ba7263
Andrei Dziaineka <dziaineka@gmail.com>
2018-02-11 18:06:16 +0300

    refactoring

--- js/placemark.js
+++ js/placemark.js
@@ -1,34 +1,34 @@
-var myMap;
+let myMap;
 ymaps.ready(initMap);
 
 function initMap() {
-  myMap = new ymaps.Map("map", {
+  myMap = new ymaps.Map('map', {
     center: [53.9, 27.56659],
-    zoom: 10
+    zoom: 10,
   });
 }
 
 function getCityColor() {
   if (mansTurn) {
     return 'islands#redDotIconWithCaption';
-  } else {
-    return 'islands#greenDotIconWithCaption';
   }
+
+  return 'islands#greenDotIconWithCaption';
 }
 
 function swichToBigScale() {
   // Найдем координаты Минска и покажем его на карте
   // на маленьком уровне масштабирования.
-  ymaps.geocode('Минск').then(function (res) {
-    var coords = res.geoObjects.get(0).geometry.getCoordinates();
-    myMap.zoomRange.get(coords).then(function (range) {
+  ymaps.geocode('Минск').then((res) => {
+    const coords = res.geoObjects.get(0).geometry.getCoordinates();
+    myMap.zoomRange.get(coords).then((/* range */) => {
       myMap.setCenter(coords, 2);
     });
   });
 }
 
 function addCityToMap(cityName) {
-  var colour = getCityColor();
+  const colour = getCityColor();
   // Поиск координат центра города.
   ymaps.geocode(cityName, {
     /**
@@ -36,14 +36,14 @@ function addCityToMap(cityName) {
      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/geocode.xml
      */
     // Если нужен только один результат, экономим трафик пользователей.
-    results: 1
-  }).then(function (res) {
+    results: 1,
+  }).then((res) => {
     // Выбираем первый результат геокодирования.
-    var firstGeoObject = res.geoObjects.get(0),
-      // Координаты геообъекта.
-      coords = firstGeoObject.geometry.getCoordinates(),
-      // Область видимости геообъекта.
-      bounds = firstGeoObject.properties.get('boundedBy');
+    const firstGeoObject = res.geoObjects.get(0);
+    // Координаты геообъекта.
+    // const coords = firstGeoObject.geometry.getCoordinates();
+    // Область видимости геообъекта.
+    const bounds = firstGeoObject.properties.get('boundedBy');
 
     firstGeoObject.options.set('preset', colour);
     // Получаем строку с адресом и выводим в иконке геообъекта.
@@ -54,7 +54,7 @@ function addCityToMap(cityName) {
     // Масштабируем карту на область видимости геообъекта.
     myMap.setBounds(bounds, {
       // Проверяем наличие тайлов на данном масштабе.
-      checkZoomRange: true
+      checkZoomRange: true,
     });
   });
 }
